e6ec3f8d8638bd636cc1a3a87e357e8a3b9562bd
ericyounger <ericy@stud.ntnu.no>
2020-07-26 01:38:38 +0200

    added atmosphere file

---
+++ OSX_STATUS_APP/atmosphere.py
@@ -0,0 +1,17 @@
+import rumps
+import requests
+
+class Atmosphere(rumps.App):
+    @rumps.timer(10)
+    def prefs(self, _):
+        result = requests.get('http://localhost:3000/atmosphere/recent').json()[0]
+
+        temperature = result["temperature"]
+        humidity = result["humidity"]
+        print("loop")
+        self.title = "Temperature : {}, Humidity : {}".format(temperature, humidity)
+
+
+if __name__ == "__main__":
+    Atmosphere("Loading..").run()
+    
