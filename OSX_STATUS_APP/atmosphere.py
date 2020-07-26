6e9b6370cc444941d34d66438d381feaf637c433
ericyounger <ericy@stud.ntnu.no>
2020-07-26 16:24:06 +0200

    try catch added for missing connection

--- OSX_STATUS_APP/atmosphere.py
+++ OSX_STATUS_APP/atmosphere.py
@@ -4,12 +4,15 @@ import requests
 class Atmosphere(rumps.App):
     @rumps.timer(10)
     def prefs(self, _):
-        result = requests.get('http://localhost:3000/atmosphere/recent').json()[0]
+        try:
+            result = requests.get('http://localhost:3000/atmosphere/recent').json()[0]
 
-        temperature = result["temperature"]
-        humidity = result["humidity"]
-        print("loop")
-        self.title = "Temperature : {}, Humidity : {}".format(temperature, humidity)
+            temperature = result["temperature"]
+            humidity = result["humidity"]
+            print("loop")
+            self.title = "Temperature : {}, Humidity : {}".format(temperature, humidity)
+        except: 
+            self.title = "Not connected"
 
 
 if __name__ == "__main__":
